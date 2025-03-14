568b8556c75dda370ce1a8baee641a7a7dc7de07
Muralidharan Jeyachandaran (DHCW - Software Development) <muralidharan.jeyachandaran@wales.nhs.uk>
2025-03-14 14:27:22 +0000

    Rebase Mapper39

--- wpas-hl7-translator/src/main/java/wales/nhs/dhcw/inthub/wpasHl7/DateTimeProvider.java
+++ wpas-hl7-translator/src/main/java/wales/nhs/dhcw/inthub/wpasHl7/DateTimeProvider.java
@@ -5,14 +5,41 @@ import java.time.format.DateTimeFormatter;
 
 public class DateTimeProvider {
     public static final String DATETIME_FORMAT = "yyyyMMddHHmmss";
+    public static final String DATE_FORMAT = "yyyy-MM-dd";
+    public static final String DATE_YMD_FORMAT = "yyyyMMdd";
+    public static final String DATETIME_WITH_T_FORMAT = "yyyy-MM-dd'T'HH:mm:ss";
 
-    private DateTimeFormatter dateTimeFormatter;
+    private final DateTimeFormatter dateTimeFormatter;
+    private final DateTimeFormatter dateFormatter;
+    private final DateTimeFormatter dateyyyyMMddFormatter;
+    private final DateTimeFormatter dateTimeFormatterWithTFormat;
 
     public DateTimeProvider() {
+
         this.dateTimeFormatter = DateTimeFormatter.ofPattern(DATETIME_FORMAT);
+        this.dateFormatter = DateTimeFormatter.ofPattern(DATE_FORMAT);
+        this.dateyyyyMMddFormatter = DateTimeFormatter.ofPattern(DATE_YMD_FORMAT);
+        this.dateTimeFormatterWithTFormat = DateTimeFormatter.ofPattern(DATETIME_WITH_T_FORMAT);
+
     }
 
     public String getCurrentDatetime() {
         return LocalDateTime.now().format(dateTimeFormatter);
+    }
+
+    public DateTimeFormatter getDateTimeFormatter() {
+        return dateTimeFormatter;
+    }
+
+    public DateTimeFormatter getDateFormatter(){
+        return dateFormatter;
+    }
+
+    public DateTimeFormatter getDateyyyyMMddFormatter() {
+        return dateyyyyMMddFormatter;
+    }
+
+    public DateTimeFormatter getDateTimeFormatterWithTFormat() {
+        return dateTimeFormatterWithTFormat;
     }
 }
